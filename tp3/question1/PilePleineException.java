7c8dac8b07cb7be6581954879b625bc9ccba8877
Amine Amine <39732012+amineTree@users.noreply.github.com>
2019-04-04 00:22:30 +0300

    Update PilePleineException.java

--- tp3/question1/PilePleineException.java
+++ tp3/question1/PilePleineException.java
@@ -3,8 +3,8 @@ package question1;
 /**
  * Décrivez votre classe PilePleineException ici.
  * 
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author amine amine
+ * @version v1
  */
 public class PilePleineException extends Exception {
 }
