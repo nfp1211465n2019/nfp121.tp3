81af1c9623fa54c96bf3a40138add77305888cfb
Amine Amine <39732012+amineTree@users.noreply.github.com>
2019-04-11 22:27:51 +0300

    Update UneUtilisation.java

--- tp3/question3/UneUtilisation.java
+++ tp3/question3/UneUtilisation.java
@@ -5,10 +5,10 @@ import question1.PolygoneRegulier;
 public class UneUtilisation {
 
     public static void main(String[] args) throws Exception {
-        // déclarer p1
-        // déclarer p2
+        PileI<PolygoneRegulier> p1 = new Pile2(10);
+        PileI<PileI<PolygoneRegulier>> p2 = new Pile2(10);
 
-        // p1 est ici une pile de polygones réguliers PolygoneRegulier.java
+        // p1 est ici une pile de polygones r�guliers PolygoneRegulier.java
         p1.empiler(new PolygoneRegulier(4, 100));
         p1.empiler(new PolygoneRegulier(5, 100));
 
@@ -17,15 +17,6 @@ public class UneUtilisation {
         p2.empiler(p1);
         System.out.println(" la pile p2 = " + p2);
 
-        try {
-            // p1.empiler(new PolygoneRegulier(5,100)); // désormais une erreur de
-            // compilation
-            // ....
-            // String s = (String)p1.depiler(); // désormais une erreur de
-            // compilation
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
