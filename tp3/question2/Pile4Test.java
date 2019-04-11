a46c99141a6f34b33decea3c8737ed01fbfb76eb
Amine Amine <39732012+amineTree@users.noreply.github.com>
2019-04-11 22:37:13 +0300

    Update Pile4Test.java

--- tp3/question2/Pile4Test.java
+++ tp3/question2/Pile4Test.java
@@ -3,58 +3,67 @@ package question2;
 /**
  * Classe-test Pile4Test.
  * 
- * @author (votre nom)
+ * @author amine amine
  * @version (un numéro de version ou une date)
- * 
- *          Les classes-test sont documentées ici :
- *          http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
- *          et sont basées sur le document © 2002 Robert A. Ballance intitulé
- *          «JUnit: Unit Testing Framework».
- * 
- *          Les objets Test (et TestSuite) sont associés aux classes à tester
- *          par la simple relation yyyTest (e.g. qu'un Test de la classe
- *          Name.java se nommera NameTest.java); les deux se retrouvent dans le
- *          même paquetage. Les "engagements" (anglais : "fixture") forment un
- *          ensemble de conditions qui sont vraies pour chaque méthode Test à
- *          exécuter. Il peut y avoir plus d'une méthode Test dans une classe
- *          Test; leur ensemble forme un objet TestSuite. BlueJ découvrira
- *          automatiquement (par introspection) les méthodes Test de votre
- *          classe Test et générera la TestSuite conséquente. Chaque appel d'une
- *          méthode Test sera précédé d'un appel de setUp(), qui réalise les
- *          engagements, et suivi d'un appel à tearDown(), qui les détruit.
  */
+
 public class Pile4Test extends junit.framework.TestCase {
-	// Définissez ici les variables d'instance nécessaires à vos engagements;
-	// Vous pouvez également les saisir automatiquement du présentoir
-	// à l'aide du menu contextuel "Présentoir --> Engagements".
-	// Notez cependant que ce dernier ne peut saisir les objets primitifs
-	// du présentoir (les objets sans constructeur, comme int, float, etc.).
+	private PileI p1;
+    private PileI p2;
 
-	/**
-	 * Constructeur de la classe-test Pile4Test
-	 */
-	public Pile4Test() {
-	}
+    /**
+     * Constructeur de la classe-test Pile3Test
+     */
+    public Pile4Test() {
+    }
 
-	/**
-	 * Met en place les engagements.
-	 * 
-	 * Méthode appelée avant chaque appel de méthode de test.
-	 */
-	protected void setUp() // throws java.lang.Exception
-	{
-		// Initialisez ici vos engagements
+    /**
+     * Met en place les engagements.
+     * 
+     * Méthode appelée avant chaque appel de méthode de test.
+     */
+    protected void setUp() // throws java.lang.Exception
+    {
+        p1 = new question2.Pile();
+        p2 = new question2.Pile();
 
-	}
+    }
 
-	/**
-	 * Supprime les engagements
-	 * 
-	 * Méthode appelée après chaque appel de méthode de test.
-	 */
-	protected void tearDown() // throws java.lang.Exception
-	{
-		// Libérez ici les ressources engagées par setUp()
-	}
+    /**
+     * Supprime les engagements
+     * 
+     * Méthode appelée après chaque appel de méthode de test.
+     */
+    protected void tearDown() // throws java.lang.Exception
+    {
+        // Libérez ici les ressources engagées par setUp()
+    }
+    
+    public void test_Pile_equals() throws Exception {
+
+        p1.empiler(3);
+        p1.empiler(2);
+        p1.empiler(1);
+
+        p2.empiler(3);
+        p2.empiler(2);
+        p2.empiler(1);
+
+        assertTrue("égalité de deux piles ? ", p1.equals(p2));
+        assertTrue("égalité de deux piles ? ", p2.equals(p1));
+        assertTrue("égalité de deux piles ? ", p1.equals(p1));
+
+        p2.empiler(1);
+        assertFalse("égalité de deux piles ? ", p1.equals(p2));
+
+    }
+
+    /**
+     * Il ne vous reste plus qu'à définir une ou plusieurs méthodes de test. Ces
+     * méthodes doivent vérifier les résultats attendus à l'aide d'assertions
+     * assertTrue(<boolean>). Par convention, leurs noms devraient débuter par
+     * "test". Vous pouvez ébaucher le corps grâce au menu contextuel
+     * "Enregistrer une méthode de test".
+     */
 
 }
